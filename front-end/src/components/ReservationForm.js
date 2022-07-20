d9097719099783b7dacb4938904c4157f074018d
Mikhail <mikhailj.lajom@gmail.com>
2022-07-20 19:05:50 +0100

    US-02 finished and tests all pass

--- front-end/src/components/ReservationForm.js
+++ front-end/src/components/ReservationForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { createReservation } from "../utils/api";
+import { today } from "../utils/date-time";
 
 // reservation form component
 // reservations/new route
@@ -12,35 +13,89 @@ function ReservationForm() {
     mobile_number: "",
     reservation_date: "",
     reservation_time: "",
-    people: 0,
+    people: "",
   };
-
-  let history = useHistory();
-
-  // when submit is clicked go to dashboard
 
   // when cancel is clicked go back to previous page
   const cancelClick = () => {
     history.goBack();
   };
 
-  // useState variable and function
+  // useState variable and function for creating new reservation
   const [newReservation, setNewReservation] = useState(initialState);
+
+  // useState for errors
+  const [errorsList, setErrorsList] = useState([]);
+
+  // create function that validates Date for new reservation, sends errors and pushes it to useState array of errorsList
+  // if reservation is made on Tuesday pass error "Restaurant is Closed on Tuesdays"
+  // if reservation is made in the past "Only future reservations are allowed."
+
+  function dateTimeChecker() {
+    // convert date with new Date object
+    // get day of converted date
+    // name variable for empty array
+    // if day = 2 send error message
+    // if date > then today send error message
+
+    let errorsArray = [];
+    let date = today();
+    let todaysDate = new Date(date);
+
+    // date and day checker part of function
+    const newReservationDate = newReservation.reservation_date;
+    const reservationDate = new Date(newReservationDate);
+    
+    let reservationDay = reservationDate.getUTCDay();
+
+    if (reservationDay === 2) {
+      errorsArray.push({ message: "Restaurant is closed on Tuesdays." });
+    }
+    if (reservationDate < todaysDate) {
+      errorsArray.push({ message: "Only future reservations are allowed." });
+    }
+
+    // time checker part of function
+    // create variables for hours and minutes of reservationDate
+
+    let reservationHour = newReservation.reservation_time;
+    let reservationMins = newReservation.reservation_time;
+
+    // create if statements comparing
+
+    if (errorsArray.length === 0) {
+      return true;
+    } else {
+      setErrorsList(errorsArray);
+      return false;
+    }
+  }
+
+  // map through list of errors
+  const mappedErrors = errorsList.map((oneError, index) => {
+    if (errorsList.length === 0) {
+      return null;
+    }
+    return <li key={index}>{oneError.message}</li>;
+  });
 
   // submit handler function
   // after submitting should send data to backend
   // also should go back to dashboard with reservations list
+  // when submit is clicked go to dashboard with submitted reservation date
   async function submitHandler(evt) {
+    evt.preventDefault();
     try {
-      evt.preventDefault();
-      const abortController = new AbortController();
-      const response = await createReservation(
-        { data: newReservation },
-        abortController.signal
-      );
-      history.push(`/dashboard?date=${newReservation.reservation_date}`);
+      if (dateTimeChecker()) {
+        const abortController = new AbortController();
+        const response = await createReservation(
+          { data: newReservation },
+          abortController.signal
+        );
+        history.push(`/dashboard?date=${newReservation.reservation_date}`);
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
@@ -60,6 +115,9 @@ function ReservationForm() {
     });
   }
 
+  // allows to call the useHistory function
+  let history = useHistory();
+
   // the format of the form
   // styling if possible using bootstrap
   // requires 6 fields first_name,last_name,mobile_number,reservation_date,reservation_time, & people
@@ -67,6 +125,15 @@ function ReservationForm() {
   return (
     <>
       <fieldset>
+        <h1>Create Reservation</h1>
+        <div>
+          {errorsList.length > 0 ? (
+            <div className="alert alert-danger m-2">
+              <p>Please fix the following errors:</p>
+              <ul>{mappedErrors}</ul>
+            </div>
+          ) : null}
+        </div>
         <form onSubmit={submitHandler}>
           <div className="form-row">
             <div className="form-group col-md-4">
@@ -110,12 +177,12 @@ function ReservationForm() {
                 className="form-control"
                 value={newReservation.mobile_number}
                 maxLength="12"
-                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                // pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               />
             </div>
           </div>
           <div className="form-row">
-            <div className="form-group col-md-5">
+            <div className="form-group col-md-4">
               <label>Reservation Date</label>
               <br></br>
               <input
@@ -128,7 +195,7 @@ function ReservationForm() {
                 value={newReservation.reservation_date}
               />
             </div>
-            <div className="form-group col-md-5">
+            <div className="form-group col-md-4">
               <label>Reservation Time</label>
               <br></br>
               <input
@@ -141,7 +208,7 @@ function ReservationForm() {
                 value={newReservation.reservation_time}
               />
             </div>
-            <div className="form-group col-md-2">
+            <div className="form-group col-md-4">
               <label>People</label>
               <br></br>
               <input
@@ -158,7 +225,7 @@ function ReservationForm() {
           </div>
           <button
             type="button"
-            className="btn btn-light border mr-2"
+            className="btn btn-secondary mr-2"
             onClick={cancelClick}
           >
             Cancel
